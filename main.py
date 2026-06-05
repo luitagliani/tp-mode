9678591a344a0f9bfad1023899e3a3df942998b4
luitagliani <taglianiluisina@gmail.com>
2026-06-05 17:49:04 -0300

    punto 2

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-
-# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ==
-# 1) Calcular la órbita Lunar. Validar perigeo, apogeo, velocidad máxima y mínima.
-# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ==
+import pandas as pd
 
 GM_T = 398600.4418   # Constante de gravitacion universal (G) * masa de la tierra (MT) => (km^3/s^2)
+GM_L = 4902.8000 # Constante de gravitacion (G) * masa de la Luna (ML) => (km^3/s^2)
 RADIO_PERIGEO = 362600.0 # km
 RADIO_APOGEO  = 405400.0 # km
 SEGUNDOS_POR_DIA = 24 * 3600
 A_LUNA = (RADIO_PERIGEO + RADIO_APOGEO) / 2 #semieje mayor de órbita de la Luna
 V_PER_LUNA = math.sqrt(GM_T * (2/RADIO_PERIGEO - 1/A_LUNA)) # velocidad maxima de la Luna en km/s
 V_APO_LUNA = math.sqrt(GM_T * (2/RADIO_APOGEO - 1/A_LUNA))  # velocidad mínima de la Luna en km/s
+ANCHO = 50
 
 """
 Recibe estado = [x, y, vx, vy], donde x e y representan la posicion de la Luna
@@ -72,8 +71,42 @@ def graficar_resultados_luna(x_rk2, y_rk2, dist_rk2, tiempos_dias, r_apogeo, r_p
     plt.tight_layout()
     plt.show()
 
+"""
+Lee el archivo CSV, filtra por un rango de tiempo especificado y 
+devuelve el módulo de la distancia y la velocidad del primer registro.
+"""
+def extraer_condiciones_iniciales(ruta_archivo, inicio_str, fin_str):
+    df = pd.read_csv(ruta_archivo, sep=';', header=None, decimal=',')
+    df.columns = ['Timestamp', 'X', 'Y', 'Z', 'VX', 'VY', 'VZ']
+
+    df['Timestamp'] = pd.to_datetime(df['Timestamp'], format='%Y-%m-%dT%H:%M:%S,%f')
+
+    filtro = (df['Timestamp'] >= inicio_str) & (df['Timestamp'] <= fin_str)
+    df_filtrado = df.loc[filtro]
+
+    if df_filtrado.empty:
+        raise ValueError("No se encontraron datos en esa franja horaria.")
+
+    primer_fila = df_filtrado.iloc[0]
+
+    r_vec = np.array([primer_fila['X'], primer_fila['Y'], primer_fila['Z']])
+    v_vec = np.array([primer_fila['VX'], primer_fila['VY'], primer_fila['VZ']])
+
+    d_T = np.linalg.norm(r_vec)
+    v_0 = np.linalg.norm(v_vec)
+
+    print()
+    print("⊹₊˚‧︵‿₊୨✧୧₊‿︵‧˚₊⊹".center(ANCHO))
+    print(f"Condiciones iniciales en {primer_fila['Timestamp']}".center(ANCHO))
+    print("PUNTO 2".center(ANCHO))
+    print("- - - - - - - - - -".center(ANCHO))
+    print(f"➤ Distancia a la Tierra: {d_T:.2f} km")
+    print(f"➤ Velocidad inicial: {v_0:.3f} km/s\n")
+
+    return d_T, v_0
 
 def main():
+    #PUNTO 1
     estado_inicial = np.array([RADIO_PERIGEO, 0.0, 0.0, V_PER_LUNA])
 
     dt = 500 # Paso de integración de 8 minutos aprox
@@ -95,12 +128,10 @@ def main():
 
         estado_rk2 = paso_rk2(estado_rk2, dt)
 
-    ancho = 50
-
-    print("⊹₊˚‧︵‿₊୨✧୧₊‿︵‧˚₊⊹".center(ancho))
-    print("Resultados órbita lunar en un mes".center(ancho))
-    print("PUNTO 1".center(ancho))
-    print("- - - - - - - - - -".center(ancho))
+    print("⊹₊˚‧︵‿₊୨✧୧₊‿︵‧˚₊⊹".center(ANCHO))
+    print("Resultados órbita lunar en un mes".center(ANCHO))
+    print("PUNTO 1".center(ANCHO))
+    print("- - - - - - - - - -".center(ANCHO))
 
     print(f"➤ Apogeo real esperado: {RADIO_APOGEO} km")
     print(f"➤ Apogeo calculado con RK2: {max(dist_rk2):.2f} km\n")
@@ -115,6 +146,15 @@ def main():
     print(f"➤ Velocidad Mínima RK2: {min(vel_rk2):.4f} km/s")
 
     graficar_resultados_luna(x_rk2, y_rk2, dist_rk2, tiempos_dias, RADIO_APOGEO, RADIO_PERIGEO)
+
+    #PUNTO 2
+    archivo_datos = 'Artemis-II-Data.csv'
+
+    d_T_orion_0, v_0_orion_0 = extraer_condiciones_iniciales(
+        archivo_datos,
+        '2026-04-03 04:00:00',
+        '2026-04-03 06:00:00'
+    )
 
 
 if __name__ == "__main__":
